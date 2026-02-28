dd8e1e2b625d634accd1a47feabdb7b0bfb93a4c
avhis-hek7 <sharmaavishek679@gmail.com>
2026-02-28 17:37:04 +0545

    hash password before save in db and comapre password is done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
     email:{
         type: String,
@@ -19,7 +20,7 @@ const userSchema = mongoose.Schema({
     password:{
         type: String,
         required:[true, "Password is required for creating a account"],
-        minlength: [6, "Password should contain more 6 character"],
+        minlength: [6, "Password should contain more than 6 character"],
         select:false,
 
     }
@@ -28,4 +29,20 @@ const userSchema = mongoose.Schema({
 },{
     timestamps:true
 })
+userSchema.pre("save", async function(next){
+    if(!this.isModified('password')){
+        return next()
+    }
+    const hash = await bcrypt.hash(this.password,10)
+    this.password = hash
+    return next()
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user",userSchema);
+
+module.exports = userModel;
 
