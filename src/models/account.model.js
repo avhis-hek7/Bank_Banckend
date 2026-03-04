7e9cf238b6b69fe01e937133745564c21d5ce7a5
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-04 12:48:37 +0545

    create a accounts route ,controller and user can create accounts if its is verified {tokrn}

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -8,11 +8,14 @@ const accountSchema = new mongoose.Schema({
         index: true
     },
     status:{
+        type: String,
         enum: {
             values:["ACTIVE", "FROZEN", "CLOSED"],
-            message:"Status can be either ACTIVE, FROZEN or CLOSED"
+            message:"Status can be either ACTIVE, FROZEN or CLOSED",
+            
 
-        }
+        },
+        default:"ACTIVE"
     },
     currency:{
         type:String,
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth.middleware')
+const accountController = require('../controllers/account.controller')
 
 const router = express.Router();
+
+// POST/api/accounts/
+router.post('/', authMiddleware.authMiddleware, accountController.createAccountController )
 
 
 
