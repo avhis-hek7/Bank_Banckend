20aa23b783364a6eccdb3225853787c207a292f7
avhis-hek7 <sharmaavishek679@gmail.com>
2026-03-05 12:16:00 +0545

    create a transaction(pending)

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -2,6 +2,7 @@ const transactionModel = require('../models/transaction.model')
 const ledgerModel = require('../models/ledger.model')
 const accountModel = require('../models/account.model')
 const emailService = require('../services/email.service')
+const mongoose = require('mongoose');
 
 /** create a new transaction
  *  1.validate request
@@ -80,6 +81,19 @@ async function createTransaction(req,res){
             message:`Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}.`
         })
      }
+
+     // 5. create a transaction(pending)
+
+     const session = await mongoose.startSession()
+     session.startTransaction()
+
+     const transaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status:"PENDING"
+     },{ session })
 
 
 
